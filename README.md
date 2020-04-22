ac1a7e2840e5cbeffc50010ef7ea86ff666ee292
RichardAV <45517166+RichardAV@users.noreply.github.com>
2020-04-22 01:32:49 -0400

    Se modifico el html para generar el contenido por js. Se crea ciclo "for" que inserta info desde JSON

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # medios-informativos-chilenos
+
+Trabajaré como archivo principal index.hmtl pero el contenido será insertado por scripts.js
+
+OBJETIVOS:
+
+Crear componentes para cada medio.
+Distribuir los componentes con display:flex;
--- scripts.js
+++ scripts.js
@@ -14,9 +14,15 @@ let imprimirDatos = function(){
             
             let datos = JSON.parse(this.responseText);
             console.log(datos);
-  
+
+            for(i=0;i<=datos.length-1;i++){
+                document.write(`<h3>${datos[i].Nombre}<br></h3>`);
+                document.write(`${datos[i].Descripción}<br>`);
+                document.write(`<a href=\\"${datos[i].Web}">${datos[i].Web}</a><br>`);
+                document.write(`<br>`);
+            }            
         } 
-    }
+    }    
 }
 
 imprimirDatos();
